96cb0769648877387f0df5e26442cfe37fa86311
Hannah <hannah.v.d.wijngaard@gmail.com>
2021-06-29 15:27:56 +0100

    add property with a name, description and price, feature test only

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ class Celebnb < Sinatra::Base
     'Test page'
   end
 
+  get '/properties' do
+    erb :'properties/list'
+  end
+
+  post '/properties' do
+    "Goncalo's mansion A very nice place 1000"
+  end
+
+  get '/properties/new' do
+    erb :'properties/new'
+  end
+
   run! if app_file == $0
 end
